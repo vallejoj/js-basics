f5f5c4bc1efc4012a96cf2855e9244ce15ec8aac
Joshua Vallejo <joshua.a.vallejo@gmail.com>
2017-02-03 20:25:12 -1000

    Practice

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 /* Variables and Data Types */
 /*Console.log each variable and test your code in the terminal using the Node REPL*/
-
 /*i.e.*/
-
 var pet = "sebastian the pug";
 console.log(pet);
-
 /**
-* #1
-* Variables with a String value
-*
-* Declare variables named `firstName`, `lastName`, `birthPlace`, `favoriteFood`, `bestMovie`, `favLoveSong`, `cuteAnimal`, `favColor`, `favSneaker`, `coffeeShop`.
-* Assign values to these variables with your own data.
-*
-* Each value should be a String. 
-**/
+ * #1
+ * Variables with a String value
+ *
+ * Declare variables named `firstName`, `lastName`, `birthPlace`, `favoriteFood`, `bestMovie`, `favLoveSong`, `cuteAnimal`, `favColor`, `favSneaker`, `coffeeShop`.
+ * Assign values to these variables with your own data.
+ *
+ * Each value should be a String. 
+ **/
 var firstName = ' Joshua';
 var lastName = ' Vallejo';
 var birthPlace = ' Sacramento';
@@ -25,84 +22,74 @@ var cuteAnimal = ' Monkeys';
 var favColor = 'Grey';
 var favSneaker = 'Chucks';
 var coffeeShop = 'The Manifest';
-
 console.log(favColor);
-
 /**
-* #2
-* Variables with a Number value
-*
-* Declare variables named `favoriteNumber`, `currentYear`, `thatOnePrinceSong`, 'tokyoOlympics`, `mariahCareyAge`, `currentTemp`, `studentsInClass`, `numOfJapanPrefectures`, `currentIphoneModel` and `shoeSize`.     
-*
-* Each value should be a Number
-**/
-var favoriteNumber = 2; 
+ * #2
+ * Variables with a Number value
+ *
+ * Declare variables named `favoriteNumber`, `currentYear`, `thatOnePrinceSong`, 'tokyoOlympics`, `mariahCareyAge`, `currentTemp`, `studentsInClass`, `numOfJapanPrefectures`, `currentIphoneModel` and `shoeSize`.     
+ *
+ * Each value should be a Number
+ **/
+var favoriteNumber = 2;
 var currentYear = 2017;
 var thatOnePrinceSong = 1987;
 var tokyoOlympics = 2020;
-var mariahCareyAge = 45; 
+var mariahCareyAge = 45;
 var currentTemp = 68;
 var studentsInClass = 12;
-var numOfJapanPrefectures = 45; 
-var currentIphoneModel = 7; 
+var numOfJapanPrefectures = 45;
+var currentIphoneModel = 7;
 var shoeSize = 10;
-
-
 /**
-* #3
-* Variables with a Boolean value
-*
-* Declare variables named `likesMcDonalds`, `watchedMrRobot`, `ranMarathon`, `wrestledABear`, `lovesDonuts`, `readHarryPotter`, `sleptInClass`, `drinksCoffee`, `shopsAtWholeFoods` and `ownsRedShoes`.     
-*
-* Each value should be a Boolean
-**/
-var likesMcDonalds= false;
-var watchedMrRobot= true;
-var ranMarathon= false; 
-var wrestledABear= false;
-var lovesDonuts= true;
-var readHarryPotter= true; 
-var sleptInClass= false; 
-var drinksCoffee= true;
-var shopsAtWholeFoods= false;
-var ownsRedShoes= false; 
+ * #3
+ * Variables with a Boolean value
+ *
+ * Declare variables named `likesMcDonalds`, `watchedMrRobot`, `ranMarathon`, `wrestledABear`, `lovesDonuts`, `readHarryPotter`, `sleptInClass`, `drinksCoffee`, `shopsAtWholeFoods` and `ownsRedShoes`.     
+ *
+ * Each value should be a Boolean
+ **/
+var likesMcDonalds = false;
+var watchedMrRobot = true;
+var ranMarathon = false;
+var wrestledABear = false;
+var lovesDonuts = true;
+var readHarryPotter = true;
+var sleptInClass = false;
+var drinksCoffee = true;
+var shopsAtWholeFoods = false;
+var ownsRedShoes = false;
 /**
-* #4
-* Variables with a Null value
-*
-* Declare variables named `completedPrepClass`, `traveledToMars` and `scoredTDinNFL`.
-*
-* Each value should be Null 
-**/
-var completedPrepClass= null;
-var traveledToMars= null; 
-var scoredTDinNFL= null; 
-
+ * #4
+ * Variables with a Null value
+ *
+ * Declare variables named `completedPrepClass`, `traveledToMars` and `scoredTDinNFL`.
+ *
+ * Each value should be Null 
+ **/
+var completedPrepClass = null;
+var traveledToMars = null;
+var scoredTDinNFL = null;
 /*
 #5. 
 * What is the difference between null and undefined? Type your answer in two or three commented-out sentences.
 *Undefined is when you default to zero, null is when you make an active choice for that variable to be zero.**/
-
-
-
 /**
-* #6
-* Variables with an Array value
-* 
-* Declare a variable named 'plateLunch` and assign it an array containing 5 of your favorite lunch items.
-* Declare a variavle named `fruitBasket` and assign it an array containing 5 fruits of your choosing.
-* Declare a variable named `westCoast` and assign it an array containing states found on the west coast of the US.
-* Declare a variable named `lotteryResult and assign it an array containing 5 random numbers.
-* Declare a variable named `duets` and assign it an array containing 3 nested arrays, each containing the names of 2 singers or bands.
-* Declare a variable named `mixPlate` and assign it an array containing different data types of your choosing.
-**/
-
-var plateLunch = [11,12,1,2,3];
-var fruitBasket = ['banana','apple','bacon','pineapple','grapess'];
-var westCoast = ['CA','WA','OR','NV','AZ'];
-var lotteryResult = [5,6,7,8,2344];
-var duets= [['Beatles, Beach Boys'],['Madonna, Cher'],['Ringo, John']];
-
+ * #6
+ * Variables with an Array value
+ * 
+ * Declare a variable named 'plateLunch` and assign it an array containing 5 of your favorite lunch items.
+ * Declare a variavle named `fruitBasket` and assign it an array containing 5 fruits of your choosing.
+ * Declare a variable named `westCoast` and assign it an array containing states found on the west coast of the US.
+ * Declare a variable named `lotteryResult and assign it an array containing 5 random numbers.
+ * Declare a variable named `duets` and assign it an array containing 3 nested arrays, each containing the names of 2 singers or bands.
+ * Declare a variable named `mixPlate` and assign it an array containing different data types of your choosing.
+ **/
+var plateLunch = [11, 12, 1, 2, 3];
+var fruitBasket = ['banana', 'apple', 'bacon', 'pineapple', 'grapess'];
+var westCoast = ['CA', 'WA', 'OR', 'NV', 'AZ'];
+var lotteryResult = [5, 6, 7, 8, 2344];
+var duets = [['Beatles, Beach Boys'], ['Madonna, Cher'], ['Ringo, John']];
 /*7
 * Accessing Values in Arrays
 
@@ -112,12 +99,10 @@ What is the length of this array? Please print the length (a number) to the cons
 What is the index position of carrot cake? Please print this item to the console.0
 What is the index position of lemon bars? Please print this item to the console.4
 */
-
 var partyList = ["carrot cake", "rocky road ice cream", "mochiko chicken", "orange tang", "lemon bars"];
 console.log(partyList.length);
 console.log(partyList[0]);
 console.log(partyList[4]);
-
 /*
 * #8 
 * Concatenation 
@@ -132,20 +117,15 @@ console.log(partyList[4]);
 At the age of xx, Mariah Carey, wearing her fancy xx, sang xx at the xx.
 */
 var fullName = firstName.concat(lastName);
-
-
-var whereFrom= fullName.concat (' is from', birthPlace);
-
-var mariahStory = 'at the age of'.concat(mariahCareyAge, 'wearing her fancy', favSneaker,'sang', favLoveSong, 'at the', coffeeShop);
+var whereFrom = fullName.concat(' is from', birthPlace);
+var mariahStory = 'at the age of'.concat(mariahCareyAge, 'wearing her fancy', favSneaker, 'sang', favLoveSong, 'at the', coffeeShop);
 console.log(mariahStory);
-
-var num1= 2;
-var num2= 10;
-var sum= num1 + num2;
-var difference= num2 - num1;
-var product= num1 * num2;
-var quotient= num2 / num1;
-
+var num1 = 2;
+var num2 = 10;
+var sum = num1 + num2;
+var difference = num2 - num1;
+var product = num1 * num2;
+var quotient = num2 / num1;
 console.log(quotient);
 /**
 * #9 Arithmetic
@@ -160,21 +140,15 @@ console.log(quotient);
 * What is the remainder when you divide num1 by num2? Print your answer to the console.
 
 /**/
-
-var isStrictlyEqual= 'Tacocat'==='Tacocat';
-var isLooselyEqual= 'Tacocat' == 'tacocat' ;
+var isStrictlyEqual = 'Tacocat' === 'Tacocat';
+var isLooselyEqual = 'Tacocat' == 'tacocat';
 console.log(isStrictlyEqual);
 console.log(isLooselyEqual);
-
-var sameNum= '5' === 5; 
-var sameNumba= '5' == 5;
+var sameNum = '5' === 5;
+var sameNumba = '5' == 5;
 console.log(sameNum);
 console.log(sameNumba);
-
-    
-    
-    
-    /**
+/**
 * #10
 * Comparisons & Logic
 * Variables with the outcome of a comparison operation:
@@ -194,9 +168,7 @@ console.log(sameNumba);
 * Declare variables and print the results to the console. What are the differences between the three and why?
 
 /**/
-var mybox= 5;
-
-
+var mybox = 5;
 /**
 * #11
 * Assignments
@@ -210,30 +182,26 @@ var mybox= 5;
 * assignment statement to show the current value. REMEMBER: an assignment is DIFFERENT from a declaration.
 *
 **/
-function bakePie(ingredient){
-   return "Today's special is" + ingredient + " pie.";
+function bakePie(ingredient) {
+    return "Today's special is" + ingredient + " pie.";
 }
-
-
-var pieResult= bakePie(' apple');
-console.log (pieResult);
+var pieResult = bakePie(' apple');
+console.log(pieResult);
 /**
-* Final Boss 
-* Additional exercises designed to further challenge you and give you a jump start on the next topic that'll be covered.
-* Function - A block of code designed to perform a particular task. It allows you to reuse code.
-*
-* Declare a function named `bakePie` which has a single parameter 'ingredient'.
-* When the function is invoked, it should return a string, "Today's special ingredient pie." Where ingredient will be the value that you pass into the function.
-* For example, if the value is `blueberry`, it should return "Today's special blueberry pie."
-* Declare a variable 'pieResult' and assign it to the function call.
-* Console.log the variable to see the result.**/
-
-function jump(height){
+ * Final Boss 
+ * Additional exercises designed to further challenge you and give you a jump start on the next topic that'll be covered.
+ * Function - A block of code designed to perform a particular task. It allows you to reuse code.
+ *
+ * Declare a function named `bakePie` which has a single parameter 'ingredient'.
+ * When the function is invoked, it should return a string, "Today's special ingredient pie." Where ingredient will be the value that you pass into the function.
+ * For example, if the value is `blueberry`, it should return "Today's special blueberry pie."
+ * Declare a variable 'pieResult' and assign it to the function call.
+ * Console.log the variable to see the result.**/
+function jump(height) {
     return " You jumped height " + height + " feet high";
 }
-var jumpResult= jump( 4);
-console.log (jumpResult);
-
+var jumpResult = jump(4);
+console.log(jumpResult);
 /**
 * Declare a Function named `jump` which has a single parameter, `height`.
 * This function should return a string, "You jumped height feet high!" Where height will be the value that you pass into the function.
@@ -247,4 +215,3 @@ console.log (jumpResult);
 * This function should return a String along the lines of, "Tomatoes and Cheese make a Pizza!"
 * if invoked in this way -> cook("Tomatoes", "Cheese", "Pizza");
 **/
-
